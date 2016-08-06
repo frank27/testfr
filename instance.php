77ecbb93566446614259701c4df7543592b6b84f
Franklin Palomino Camacho <frank.fr.27@gmail.com>
2016-08-06 17:53:40 -0500

    instance

--- instance.php
+++ instance.php
@@ -1,8 +1,8 @@
 <?php
-$this_instance_id=file_get_contents('http://172.31.59.77/latest/meta-data/instance-id');
+$this_instance_id=file_get_contents('http://169.254.169.254/latest/meta-data/');
 
 if(!empty($this_instance_id)){
-    echo (string)($this_instance_id);
+    var_dump($this_instance_id);
 }
 else{
     echo "Sorry, instance id unknown";
