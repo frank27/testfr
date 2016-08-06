f04cf6552058af8f75a62c61236ddf22bbab0f3b
Franklin Palomino Camacho <frank.fr.27@gmail.com>
2016-08-06 18:09:19 -0500

    fixs

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  * User: Zero
  * Date: 06/08/2016
  * Time: 04:51 PM
+ * http://web-tier-elb-417854062.us-east-1.elb.amazonaws.com/
  */
 echo $_SERVER['REMOTE_ADDR'];
 echo "<br>";
--- instance.php
+++ instance.php
@@ -1,5 +1,8 @@
 <?php
-$this_instance_id=file_get_contents('http://169.254.169.254/latest/meta-data/');
+//inside curl http://169.254.169.254/latest/meta-data/
+
+//outside
+$this_instance_id=file_get_contents('http://169.254.169.254/latest/meta-data/public-hostname');
 
 if(!empty($this_instance_id)){
     var_dump($this_instance_id);
